2fcc3dfe4db16ce65874e32c25672f7f8e1d0bb0
Solomon Bhaskar <131343533+solomon712@users.noreply.github.com>
2025-09-17 14:41:51 +0530

    Update useAuth.js

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -22,7 +22,7 @@ const useAuth = () => {
     localStorage.setItem('group', JSON.stringify(userData.group));
     localStorage.setItem('job_title', JSON.stringify(userData.job_title));
     localStorage.setItem('token', userData.token);
-    localStorage.setItem('login_session_id', JSON.stringify(userData.login_session_id)); // ✅ Correct
+    localStorage.setItem('login_session_id', JSON.stringify(loginSessionId));
   };
 
   const logoutUser = () => {
